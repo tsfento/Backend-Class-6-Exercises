ad0f867bd2c2cf3e00d6c8d3a609e099bd5e7784
Tyler Fenton <tsfento@gmail.com>
2024-01-25 13:14:24 -0600

    init commit

---
+++ sql_exercise_1.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Customers (
+    CustomerID int,
+    Name text,
+    Email text
+);
+
+INSERT INTO Customers (CustomerID, Name, Email)
+VALUES (1, 'Alice Johnson', 'alice.johnson@email.com'),
+       (2, 'Bob Smith', 'bob.smith@email.com'),
+       (3, 'Charlie Brown', 'charlie.brown@email.com');
+
+SELECT * FROM Customers;
